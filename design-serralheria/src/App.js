353dd4aa5d2ad74a4c6db10f13cc71c46587ce7a
Bonnington Dublin <147234368+BonningtonDublin@users.noreply.github.com>
2024-09-16 13:38:09 +0100

    3

--- design-serralheria/src/App.js
+++ design-serralheria/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import FormPage from "./FormPage";
+import Home from "./Home/Home";
+import FormPage from "./Formulario/FormPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/formulario" element={<FormPage />} />
+        <Route path="/design-serralheria/" element={<Home />} />
+        <Route path="/design-serralheria/formulario" element={<FormPage />} />
       </Routes>
     </Router>
   );
