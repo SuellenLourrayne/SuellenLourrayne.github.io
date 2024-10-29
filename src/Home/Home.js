2ae8ba6cb13ee5c1191baca895697c7fc961e9d7
Suellen Lourrayne <lourraynedeoliveira@gmail.com>
2024-10-29 18:19:08 +0000

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -16,9 +16,9 @@ const Home = () => {
       <section id="about" className="about">
         <div className="form-container">
             <h2>About Me</h2>
-            <p>My name is Suellen Lourrayne. I am from Brazil and have been captivated by the art of development since my teenage years. 
-            <br>At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. 
-            <br>Here, you will find a collection of my work and projects that reflect my journey so far.</p>
+            <p>My name is Suellen Lourrayne. I am from Brazil and have been captivated by the art of development since my teenage years. </p>
+            <p>At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. </p>
+            <p>Here, you will find a collection of my work and projects that reflect my journey so far.</p>
         </div>
       </section>
 
