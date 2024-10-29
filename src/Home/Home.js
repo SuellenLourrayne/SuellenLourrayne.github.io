753c0d20bb2dc4fbd7e5cdac87f26ecce2f65551
Suellen Lourrayne <lourraynedeoliveira@gmail.com>
2024-10-29 18:15:43 +0000

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
             <h2>About Me</h2>
             <p>My name is Suellen Lourrayne. I am from Brazil and have been captivated by the art of development since my teenage years. 
             <br>At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. 
-            <br>Here, you&apos;ll find a collection of my work and projects that reflect my journey so far.</p>
+            <br>Here, you will find a collection of my work and projects that reflect my journey so far.</p>
         </div>
       </section>
 
