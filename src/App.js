fb48ed80fecdf24ca80a52dcae79bc6e661d30c1
Bonnington Dublin <lourraynedeoliveira@gmail.com>
2025-02-21 14:05:45 +0000

    1

--- src/App.js
+++ src/App.js
@@ -7,26 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <><div className="App">
-      <header className="App-header">
-        <nav className="App-nav">
-          <ul>
-            <li><a href="/">Início</a></li>
-            <li><a href="/Projects">Projects</a></li>
-            <li><a href="/Contact">Contact</a></li>
-          </ul>
-        </nav>
-
-        <div className="description col-6">
-          <h2>Suellen Lourrayne - Professional Portfolio</h2>
-		        <p>I am from Brazil and have been captivated by the art of development since my teenage years. 
-              At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. 
-		          Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. 
-              Here, you will find a collection of my work and projects that reflect my journey so far.</p>
-        </div>
-
-      </header>
-    </div>
+    <>
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -13,11 +13,24 @@ const Home = () => {
   return (
     <div className="App">
 
-      <section id="about" className="about">
-        <div className="form-container">
-			zzz
+      <header className="App-header">
+        <nav className="App-nav">
+          <ul>
+            <li><a href="/">Início</a></li>
+            <li><a href="/Projects">Projects</a></li>
+            <li><a href="/Contact">Contact</a></li>
+          </ul>
+        </nav>
+
+        <div className="description col-6">
+          <h2>Suellen Lourrayne - Professional Portfolio</h2>
+		        <p>I am from Brazil and have been captivated by the art of development since my teenage years. 
+              At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. 
+		          Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. 
+              Here, you will find a collection of my work and projects that reflect my journey so far.</p>
         </div>
-      </section>
+
+      </header>
 
     </div>
   );
