fd0bf3739040244620f53d079730f5741e324824
Bonnington Dublin <lourraynedeoliveira@gmail.com>
2025-02-21 13:40:50 +0000

    1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
 import Contact from "./Contact/Contact";
 import Projects from "./Projects/Projects";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
@@ -15,11 +16,15 @@ const App = () => {
             <li><a href="/Contact">Contact</a></li>
           </ul>
         </nav>
-        <h2>Suellen Lourrayne - Professional Portfolio</h2>
-		<p>I am from Brazil and have been captivated by the art of development since my teenage years. 
-        At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. 
-		Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. </p>
-        <p>Here, you will find a collection of my work and projects that reflect my journey so far.</p>
+
+        <div className="description col-6">
+          <h2>Suellen Lourrayne - Professional Portfolio</h2>
+		        <p>I am from Brazil and have been captivated by the art of development since my teenage years. 
+              At 21, I moved to Ireland to enhance my language skills and immerse myself in a new culture. 
+		          Since then, I have dedicated myself to continuous improvement, always seeking new knowledge and opportunities. </p>
+            <p>Here, you will find a collection of my work and projects that reflect my journey so far.</p>
+        </div>
+
       </header>
     </div>
     <Router>
